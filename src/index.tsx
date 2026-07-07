5c13baed142a3d59d349acdbefe343a9c5512250
pewpil <pewpil@proton.me>
2026-07-07 19:12:02 +0800

    backup commit for renewal

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Router, Route } from '@solidjs/router'
 import './pages/dvibd/styles/index.module.css'
 import App from './pages/dvibd/ui/App.tsx'
 import Login from './pages/dvibd/ui/Login.tsx'
+import Signup from './pages/dvibd/ui/Signup.tsx'
 
 const root = document.getElementById('root')
 
@@ -11,5 +12,6 @@ render(() => (
   <Router>
     <Route path="/" component={App} />
     <Route path="/login" component={Login} />
+    <Route path="/signup" component={Signup} />
   </Router>
 ), root!)
