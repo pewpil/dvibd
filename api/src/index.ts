fdb434769d6a745bb2b17cff8b33f6bc0f1972c6
pewpil <pewpil@proton.me>
2026-08-01 10:48:53 +0800

    api re-init

--- api/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "@hono/hono";
-import router from "./routes/routes.ts";
-
-const app = new Hono();
-
-app.route("/", router);
-
-Deno.serve({ port: 3001 }, app.fetch);
--- api/src/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "@hono/hono";
-import authRouter from "./auth.ts";
-
-const app: Hono = new Hono();
-
-app.route("/auth", authRouter);
-
-export default app;
