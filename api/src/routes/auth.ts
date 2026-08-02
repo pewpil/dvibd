eb67f97d25c735eff2c05cec97006c6af8a1568c
pewpil <pewpil@proton.me>
2026-08-02 16:04:14 +0800

    api login and signup endpoints implemented except TODOs

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,12 +1,16 @@
+import { compare, genSalt, hash } from "@da/bcrypt";
 import { Hono } from "@hono/hono";
-import z from "@zod/zod";
+import { z } from "@zod/zod";
 import HttpError from "../lib/error.ts";
 import db from "../orm/db.ts";
 import { Prisma, User } from "../orm/generated/client.mts";
+import registerErrorHandler from "./error.ts";
 
 const router: Hono = new Hono();
 
 type PublicUser = Omit<User, "password" | "createdAt">;
+
+const SALT_ROUNDS: number = Number(Deno.env.get("SALT_ROUNDS")) || 4;
 
 router.post("/signup", async function(c) {
   //TODO: throw an error if req.header("Content-Type") is not "application/json"
@@ -14,14 +18,20 @@ router.post("/signup", async function(c) {
   //TODO: throw an error if a NewUser property is left null or undefined.
   type NewUser = Omit<User, "id" | "createdAt">;
 
+  // TODO: throw an error if newUser.name contains non-alphanumeric and -underscore characters
   const newUser: NewUser = await c.req.json();
+
+  const passwordHash: string = await hash(
+    newUser.password,
+    await genSalt(SALT_ROUNDS),
+  );
 
   try {
     const user: PublicUser = await db.user.create({
       data: {
         name: newUser.name,
         email: newUser.email,
-        password: newUser.password,
+        password: passwordHash,
       },
       select: {
         id: true,
@@ -59,12 +69,7 @@ router.post("/signup", async function(c) {
     }
   }
 
-  return c.json(
-    {
-      error: "Unknown server error.",
-    },
-    500,
-  );
+  throw new HttpError("Unknown server error", HttpError.INTERNAL_SERVER_ERROR);
 });
 
 router.post("/login", async function(c) {
@@ -77,15 +82,17 @@ router.post("/login", async function(c) {
 
   const isEmail: boolean = z.email().safeParse(emailOrName).success;
 
-  type LoggingUser = Omit<User, "id" | "createdAt">;
+  type LoggingUser = Omit<User, "createdAt">;
   let user: LoggingUser | null;
 
+  // TODO: make sure the query is sql injection safe
   if (isEmail) {
     user = await db.user.findUnique({
       where: {
         email: emailOrName,
       },
       select: {
+        id: true,
         name: true,
         email: true,
         password: true,
@@ -97,6 +104,7 @@ router.post("/login", async function(c) {
         name: emailOrName,
       },
       select: {
+        id: true,
         name: true,
         email: true,
         password: true,
@@ -108,18 +116,19 @@ router.post("/login", async function(c) {
     throw new HttpError("Wrong username or password", HttpError.UNAUTHORIZED);
   }
 
-  // if (!(await compare(password, user.password))) {
-  //   throw new HttpError("Wrong username or password", HttpError.UNAUTHORIZED);
-  // }
-
-  if (password !== user.password) {
+  if (!(await compare(password, user.password))) {
     throw new HttpError("Wrong username or password", HttpError.UNAUTHORIZED);
   }
 
   return c.json(
-    { message: `User, ${user.name}, successfully logged in`, user: user },
+    {
+      message: `User, ${user.name}, successfully logged in`,
+      user: { id: user.id, name: user.name, email: user.email } as PublicUser,
+    },
     200,
   );
 });
+
+registerErrorHandler(router);
 
 export default router;
