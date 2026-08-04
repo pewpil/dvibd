c8c7b689c767eeacabd98ca7a9868692c82e64a8
pewpil <pewpil@proton.me>
2026-08-04 14:13:06 +0800

    re-init

--- api/src/orm/db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/client.mts";
-
-const adapter: PrismaPg = new PrismaPg({
-  connectionString: Deno.env.get("DATABASE_URL"),
-});
-
-export default new PrismaClient({ adapter });
