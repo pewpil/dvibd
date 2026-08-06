b9db1babf377f3e1b41a35d9816f93ff0b170faa
pewpil <pewpil@proton.me>
2026-08-06 10:44:14 +0800

    added logo for social and message

--- gui/src/uis/components/dvibd/home/landing/Product.tsx
+++ gui/src/uis/components/dvibd/home/landing/Product.tsx
@@ -1,6 +1,8 @@
 import { A } from '@solidjs/router'
 import type { ParentProps } from 'solid-js'
 import style from '../../../../../styles/components/dvibd/home/landing/Product.module.css'
+import socialLogo from '../../../../../assets/components/dvibd/home/landing/product/social.svg'
+import messageLogo from '../../../../../assets/components/dvibd/home/landing/product/message.svg'
 
 type ProductProps = ParentProps & {
   name: string
@@ -9,9 +11,14 @@ type ProductProps = ParentProps & {
 }
 
 function Product(props: ProductProps) {
+  const logo = props.variant === 'social' ? socialLogo : messageLogo
+
   return (
     <article classList={{ [style.productCard]: true, [style[props.variant]]: true }}>
-      <h3>{props.name}</h3>
+      <div id={style.productTitle}>
+        <img src={logo} alt={`${props.name} logo`} />
+        <h3>{props.name}</h3>
+      </div>
       <p>{props.children}</p>
       <A href={props.href}>Open {props.name}</A>
     </article>
